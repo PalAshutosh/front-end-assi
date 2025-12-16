75decde7004d6a9f623c162f2af2ca09cbfc4c49
PalAshutosh <palashutos397@gmail.com>
2025-12-16 11:09:09 +0530

    Update project and add root gitignore

--- backend/server.js
+++ backend/server.js
@@ -12,21 +12,20 @@ const authRoutes = require('./src/routes/authRoutes');
 const taskRoutes = require('./src/routes/taskRoutes');
 const { errorHandler } = require('./src/middleware/errorMiddleware');
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
+
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-// Error Middleware
+
 app.use(errorHandler);
 
-// Start Server
+
 const startServer = async () => {
     await connectDB();
-    // Sync models
+    
     await sequelize.sync();
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
